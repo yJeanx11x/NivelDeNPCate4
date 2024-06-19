e82525263bec0746d0aba56cee1e7c0d422354af
Jean Lucas <j3anlucas010@gmail.com>
2024-06-18 21:09:04 -0300

    lvl

---
+++ script.js
@@ -0,0 +1,26 @@
+let subirDeLevel = 4;
+
+switch (subirDeLevel) {
+        case 0:
+                console.log('Rank 10: Porcelana')
+               break;
+
+        case 1:
+                console.log('Rank 10: Porcelana')
+                break;
+               
+
+        case 2:
+                console.log('Rank 10: Porcelana')
+                break;
+        case 3: 
+        console.log('Rank 9: Obsidiana')
+        break;        
+        case 4:                
+        console.log('Rank 9: Obsidiana')
+
+
+        default:
+}
+
+// Rank de Aventureiros e Aventureiros NPCsRank 4: Porcelana (1-2 nível)Rank 9: Obsidiana (3-4 nível)
